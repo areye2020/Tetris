a7616d5d789aee7c4446f0ec7c912f2d1ffa0f6b
Adriana Reyes <adriana@wireless-10-148-157-152.public.utexas.edu>
2020-10-14 18:17:26 -0500

    testing for TetrisPiece

--- src/assignment/TetrisPieceTest.java
+++ src/assignment/TetrisPieceTest.java
@@ -1,5 +1,7 @@
 package assignment;
 
+import java.awt.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TetrisPieceTest {
@@ -12,65 +14,61 @@ class TetrisPieceTest {
     private TetrisPiece leftL = new TetrisPiece(Piece.PieceType.LEFT_L);
     private TetrisPiece rightL = new TetrisPiece(Piece.PieceType.RIGHT_L);
 
-
-    @org.junit.jupiter.api.Test
-    void returnRotations() {
-    }
-
     @org.junit.jupiter.api.Test
     void calculateSkirt() {
         assertArrayEquals(new int[] {2, 2, 2, 2}, stick.calculateSkirt());
-    }
-
-    @org.junit.jupiter.api.Test
-    void getRotations() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void rotateBody() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getType() {
+        assertArrayEquals(new int[] {0, 0}, square.calculateSkirt());
+        assertArrayEquals(new int[] {1, 1, 2}, rightDog.calculateSkirt());
+        assertArrayEquals(new int[] {1, 1, 1}, leftL.calculateSkirt());
     }
 
     @org.junit.jupiter.api.Test
     void getRotationIndex() {
+        assertEquals(1, rightDog.clockwisePiece().getRotationIndex());
+        assertEquals(3, rightDog.counterclockwisePiece().getRotationIndex());
     }
 
     @org.junit.jupiter.api.Test
     void clockwisePiece() {
+        assertArrayEquals(new Point[] {new Point(1,2), new Point(1, 1), new Point(1, 0),
+                new Point(2, 2)}, leftL.clockwisePiece().getBody());
+        Piece temp = leftL.clockwisePiece();
+        assertArrayEquals(new Point[] {new Point(2,1), new Point(1, 1), new Point(0, 1),
+                new Point(2, 0)}, temp.clockwisePiece().getBody());
+        temp = temp.clockwisePiece();
+        assertArrayEquals(new Point[] {new Point(1,0), new Point(1, 1), new Point(1, 2),
+                new Point(0, 0)}, temp.clockwisePiece().getBody());
+        temp = temp.clockwisePiece();
+        assertArrayEquals(new Point[] {new Point(0,1), new Point(1, 1), new Point(2, 1),
+                new Point(0, 2)}, temp.clockwisePiece().getBody());
     }
 
     @org.junit.jupiter.api.Test
     void counterclockwisePiece() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getWidth() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getHeight() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getBody() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getSkirt() {
+        assertArrayEquals(new Point[] {new Point(1,0), new Point(1, 1), new Point(1, 2),
+                new Point(0, 1)}, t.counterclockwisePiece().getBody());
+        Piece temp = t.counterclockwisePiece();
+        assertArrayEquals(new Point[] {new Point(2,1), new Point(1, 1), new Point(0, 1),
+                new Point(1, 0)}, temp.counterclockwisePiece().getBody());
+        temp = temp.counterclockwisePiece();
+        assertArrayEquals(new Point[] {new Point(1,2), new Point(1, 1), new Point(1, 0),
+                new Point(2, 1)}, temp.counterclockwisePiece().getBody());
+        temp = temp.counterclockwisePiece();
+        assertArrayEquals(new Point[] {new Point(0,1), new Point(1, 1), new Point(2, 1),
+                new Point(1, 2)}, temp.counterclockwisePiece().getBody());
     }
 
     @org.junit.jupiter.api.Test
     void testEquals() {
-    }
-
-    @org.junit.jupiter.api.Test
-    void getWallKicks() {
+        assertEquals(false, square.equals(4));
+        assertEquals(false, square.equals(t));
+        assertEquals(false, square.equals(square.clockwisePiece()));
+        Piece testSquare = new TetrisPiece(Piece.PieceType.SQUARE);
+        assertEquals(true, testSquare.equals(square));
     }
 
     @org.junit.jupiter.api.Test
     void lowestSkirt() {
+        assertEquals(2, stick.lowestSkirt();
     }
 }
